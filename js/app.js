d359d96e052e800fac9619f586b0f9ecb8b9543e
mrc4619 <mrchayon5525@gmail.com>
2022-09-03 01:55:33 +0600

    modal added

--- js/app.js
+++ js/app.js
@@ -31,22 +31,28 @@ const loadNews = (categoryId) => {
 
 }
 
+
 const displayNews = news => {
     // console.log(news)
+
+    // loader start
+    const loderSection = document.getElementById('loader');
+    loderSection.classList.remove('hidden')
 
     const cardContainer = document.getElementById('card-conatiner');
     cardContainer.textContent = '';
 
     // short by rating
     news.sort((a, b) => {
-        return b.rating.number - a.rating.number
+        return b.total_view - a.total_view
     })
+
+
 
     const foundText = document.getElementById('found-text');
     foundText.innerHTML = `<h1 class="text-center font-bold text-2xl py-4 bg-slate-200"><span class="text-rose-500">${news.length}</span> items found for this category</h1>`
     news.forEach(singleNews => {
-
-
+        // console.log(singleNews._id)
         const cardDiv = document.createElement('div');
         cardDiv.innerHTML = `
         <div class="card lg:card-side bg-base-100 w-3/4 my-4 mx-auto shadow-xl">
@@ -57,17 +63,47 @@ const displayNews = news => {
          <div class="card-actions justify-between">
             <div class="flex gap-2"> 
                <img class="w-7 h-7 rounded-full" src="${singleNews.author.img}">
-               <p class="font-semibold">${singleNews.author ? singleNews.author.name : "No Author"}</p>
+               <p class="font-semibold">${singleNews.author.name ? singleNews.author.name : "No Author"}</p>
             </div>
             <div>
-                 <p class="font-bold">Rating : <span class="text-rose-500">${singleNews.rating ? singleNews.rating.number : "No Rating"}</span></p>
+                 <p class="font-bold">Watched : <span class="text-rose-500">${singleNews.total_view ? singleNews.total_view + 'k' : "No View"}</span></p>
             </div>
-            <button class="btn btn-primary">Show Deatil</button>
-            </div>
+
+            <label onclick="showModal('${singleNews._id}')" for="my-modal-3" class="btn btn-primary modal-button">Show Deatil</label>
+            
         </div>
     </div>`;
         cardContainer.appendChild(cardDiv);
-    })
+
+
+        // loader end
+        loderSection.classList.add('hidden');
+    });
+
+
+    // modal
+    // const showModal = id => {
+    //     const url = `https://openapi.programming-hero.com/api/news/${id}`
+    //     fetch(url)
+    //         .then(res => res.json())
+    //         .then(data => console.log(data))
+    // }
+    // showModal()
+
+
+    // const showModal = async (id) => {
+    //     const url = `https://openapi.programming-hero.com/api/news/${id}`;
+    //     const res = await fetch(url);
+    //     const data = await res.json();
+    //     displayModal(data)
+    // }
+    // const displayModal = deaitlId => {
+    //     console.log(deaitlId)
+    // }
+
+
+
+
     //     cardContainer.innerHTML = `
     //         <div class="card lg:card-side bg-base-100 w-3/4 my-4 mx-auto shadow-xl">
     //             <figure><img src="${news.thumbnail_url}" alt="Album"></figure>
@@ -89,6 +125,31 @@ const displayNews = news => {
     // `;
 
 }
+
+
+
+// modal
+
+const showModal = async (id) => {
+    const url = `https://openapi.programming-hero.com/api/news/${id}`;
+    const res = await fetch(url);
+    const data = await res.json();
+    displayModal(data.data[0])
+}
+const displayModal = deaitl => {
+    console.log(deaitl);
+    const modalDiv = document.getElementById('modal-div');
+    modalDiv.innerHTML = `
+    <img src="${deaitl.image_url}" alt="">
+    <h3 class="text-lg font-bold">${deaitl.title}</h3>
+    <p class="py-4">${deaitl.details}</p>
+    <p class="font-bold">Rating : <span class="text-rose-500">${deaitl.rating.number}</span></p>
+    `;
+}
+
+
+
+
 
 // loader function
 // const isLoading = () => {
